b8ae6d183db8c0ec39bf83c644b1b57f13ffb74a
Xelvis <elvisgideonuzuegbu@gmail.com>
2024-12-12 18:06:58 +0100

    Implemented Expandable text

---
+++ src/components/ExpandableText.tsx
@@ -0,0 +1,25 @@
+import { ReactNode, useState } from "react";
+import { Button, Heading, Text } from "@chakra-ui/react";
+
+interface Props {
+    children: string;
+    maxChars?: number 
+};
+
+
+export default function ExpandableText({children: text, maxChars=300}: Props) {
+    const [isExpanded, setExpanded] = useState(false)
+
+    if (text.length <= maxChars)
+        return <Text>{text}</Text>
+    
+    const fullText = isExpanded ? text : text.slice(0, maxChars)+'...'
+    return (
+        <Text>
+            {fullText}
+            <Button onClick={() => setExpanded(!isExpanded)} colorScheme={'yellow'} size={'xs'} marginLeft={3}>
+                {isExpanded ? 'Show Less' : 'Show More'}
+            </Button>
+        </Text>
+    )
+}
--- src/routes/GameDetailsPage.tsx
+++ src/routes/GameDetailsPage.tsx
@@ -1,8 +1,11 @@
 import { useParams } from "react-router-dom"
 import fetchGameDetails from "../fetch-hooks/fetchGameDetails"
-import { Heading, Spinner, Text } from "@chakra-ui/react";
+import { Button, Heading, Spinner, Text } from "@chakra-ui/react";
+import { useState } from "react";
+import ExpandableText from "../components/ExpandableText";
 
 export default function GameDetailsPage() {
+    const [isExpanded, setExpanded] = useState(false)
     const { slug } = useParams();
     const { data: game, error, isLoading } = fetchGameDetails(slug!);
 
@@ -12,7 +15,7 @@ export default function GameDetailsPage() {
     return (
     <>
         <Heading paddingBottom={4}>{game.name}</Heading>
-        <Text>{game.description_raw}</Text>
+        <ExpandableText>{game.description_raw}</ExpandableText>
     </>
     )
 }
